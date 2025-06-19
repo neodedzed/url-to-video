ac72437bf5b39d0ffce4a33abcd27d7f6979069b
Ded Zed <neodedzed@gmail.com>
2025-06-19 17:44:15 +0530

    added ken burns effect

--- backend/utilities/video_utils.py
+++ backend/utilities/video_utils.py
@@ -1,14 +1,35 @@
-from moviepy import ImageClip, concatenate_videoclips
+from moviepy import AudioFileClip, ImageClip, concatenate_videoclips
 from pathlib import Path
 
-def create_clips(product='HP_OmniBook_X_Laptop_Snapdragon_X_Elite_X1E_78_100_20250619_102246'):
+def create_clips(
+        product='HP_OmniBook_X_Laptop_Snapdragon_X_Elite_X1E_78_100_20250619_102246',
+        audio_clip: AudioFileClip = None
+        ):
     images = list(Path(f'../scraped_products/{product}/images/').iterdir())
+    
+    duration = 3
+    if audio_clip:
+        duration = audio_clip.duration / len(images)
 
     print(images)
-    clips = [ImageClip(img).with_duration(3) for img in images]
+    clips = [ken_burns_effect(img,1.1, duration) for img in images]
 
     video = concatenate_videoclips(clips, method='compose')
 
     video.write_videofile(f'../scraped_products/{product}/output.mp4', fps=30)
 
-create_clips()
+def ken_burns_effect(image, zooom=1.1, duration=3):
+    clip = ImageClip(image)
+
+    width, hieght = clip.size
+
+    zoomed_image = clip.resized(zooom*clip.size)
+
+    animated = zoomed_image.crop(
+        x1=lambda t: int((zoomed_image.w - width) * t / duration),
+        y1=lambda t: int((zoomed_image.h - hieght) * t / duration),
+        x2=lambda t: int((zoomed_image.w - width) * t / duration + width),
+        y2=lambda t: int((zoomed_image.h - hieght) * t / duration + hieght)
+    ).set_duration(duration)
+
+    return animated
